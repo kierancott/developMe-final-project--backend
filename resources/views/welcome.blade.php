cf9117f105874b8f3b7c6bde750bd1ea9ce7d264
Kieran Cott <kmp.cott@gmail.com>
2020-06-14 02:15:04 +0100

    customise blade template to display movies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <h4>This file: ~/resources/views/welcome.blade.php</h4>
+                    <h5>This file: ~/resources/views/welcome.blade.php</h5>
                 </div>
             </div>
         </div>
