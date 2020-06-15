45a586d9f5c221373768272cd284b770e8afa8e9
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-15 16:49:49 +0100

    database seeding set up

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class)
+        // $movie=factory(App\Movie::class)->make(["name"=>"Crash", "year"=>"2004"]);
+        factory(App\Person::class, 12)->create()->each(function ($person){
+            global $movie;
+            $person->movies()->save(factory(App\Movie::class)->make());
+            $person->movies()->save(factory(App\Movie::class)->make());
+            // $person->movies()->save($movie);
+        });
+        
     }
 }
