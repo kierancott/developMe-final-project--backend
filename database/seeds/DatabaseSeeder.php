2ef6a61e954a2f57188d0d7123255a3b78c04d28
Nik Osvalds <nosvalds@gmail.com>
2020-06-16 12:07:40 +0100

    update db seeding to add some movies in common to people

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,13 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class)
-        // $movie=factory(App\Movie::class)->make(["name"=>"Crash", "year"=>"2004"]);
-        factory(App\Person::class, 12)->create()->each(function ($person){
-            global $movie;
-            $person->movies()->save(factory(App\Movie::class)->make());
-            $person->movies()->save(factory(App\Movie::class)->make());
-            // $person->movies()->save($movie);
+
+        factory(App\Movie::class,3)->create(); // create 3 movies in the DB
+        factory(App\Person::class, 12)->create()->each(function ($person) {
+            $person->movies()->createMany(factory(App\Movie::class,random_int(3,8))->make()->toArray()); // add 3 to 8 random movies to a person
+            $person->movies()->save(App\Movie::find(random_int(1,3))); // add a random common movie from the 3 created on line 16
         });
         
     }
